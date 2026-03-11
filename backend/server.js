8c555ab1ad72c76c3697b286c6d3fd3d791895fa
Kent Jester Gatchalian <jestergatchalian@gmail.com>
2026-03-11 23:36:37 +0800

    Added Railway

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: '.env' });
 
 const moodRoutes = require('./routes/moods');
 const authRoutes = require('./routes/auth');
